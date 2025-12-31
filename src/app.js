85077b39bd32f4a7d90c4f5812f85e047f13ec42
Om-Tubachi <omtubachi06@gmail.com>
2025-12-31 14:27:45 +0530

    PHASE 1: ACCOMPLISHED

--- src/app.js
+++ src/app.js
@@ -15,10 +15,25 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 // import routes
-import router from "./routes/user.routes.js"
-import videRouter from "./routes/video.routes.js"
-app.use("/api/users" , router)
+import userRouter from "./routes/user.routes.js"
+import videRouter from "./routes/video.routes.js" 
+import commentRouter from "./routes/comment.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+import healthcheckRouter from "./routes/healthcheck.routes.js"
+import likesRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+import subsciptionRouter from "./routes/subscription.routes.js"
+import tweetRouter from "./routes/tweet.routes.js"
+
+app.use("/api/users" , userRouter)
 app.use("/api/video" , videRouter)
+app.use("/api/comments", commentRouter)
+app.use("/api/dashboard", dashboardRouter)
+app.use("/api/health-check", healthcheckRouter)
+app.use("/api/like", likesRouter)
+app.use("/api/playlist", playlistRouter)
+app.use("/api/subscription", subsciptionRouter)
+app.use("/api/tweet", tweetRouter)
 
 
 // http://localhost:8000/api/v1/users/register
